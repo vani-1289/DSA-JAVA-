da799e65a936e9c816fbbe00fee681aadcc627e7
vani-1289 <vs8648711@gmail.com>
2026-08-18 22:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (19.17%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -1,29 +1,26 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-
-        int alt = 0;
-        int max = 0;
-
-        for (int i = 0; i < gain.length; i++) {
-            alt += gain[i];
-            max = Math.max(max, alt);
+        int n = gain.length;    
+        int[] alt = new int[n+1];  //prefix sum array 
+        for (int i = 0; i < n ;i++){
+            alt[i+1] = alt[i] + gain[i];
+        }
+        int maximum = alt[0];
+        for(int i = 1;i<alt.length;i++){
+            maximum = Math.max(maximum,alt[i]);
         }
 
-        return max;
+return maximum;
+
+     //     int alt = 0;                     //(Brute force) time complexity O(n^2)
+    //     int max = 0;
+
+    //     for (int i = 0; i < gain.length; i++) {
+    //         alt += gain[i];
+    //         max = Math.max(max, alt);
+    //     }
+
+    //     return max;
     
-    //     int n = gain.length;    
-    //     int[] alt = new int[n+1];  //prefix sum array 
-
-    //     for (int i = 0; i < n ;i++){
-    //         alt[i+1] = alt[i] + gain[i];
-    //     }
-
-    //     int max = alt[0];
-
-    //     for(int i = 1;i<alt.length;i++){
-    //         max = Math.max(max,alt[i]);
-    //     }
-
-    //   return max;
     }
 }
