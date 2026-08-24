da0dcc9771d8f5469fe0d4be6ce97fcdaeab0f3d
vani-1289 <vs8648711@gmail.com>
2026-08-24 19:23:59 +0530

    circularLL

---
+++ LinkedList/CircularLL.java
@@ -0,0 +1,76 @@
+public class CircularLL {
+    private Node head;
+    private Node tail;
+
+    public CircularLL(){
+        this.head = null;
+        this.tail = null;
+    }
+//--------------------------------------------------
+    public void insert(int val){
+        Node node = new Node(val);
+        if(head == null){
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
+//------------------------------------------------------
+    public void display(){
+        Node node = head;
+        if(head!=null){
+            do{
+                System.out.print(node.val + "->");
+                node = node.next;
+            }while(node!=head);
+        }
+        System.out.println("HEAD");
+        
+    }
+//-----------------------------------------------------------
+    public void delete(int val){
+        Node node = head;
+        if(node == null){
+            return;
+        }
+        if(node.val == val){
+            head = head.next;
+            tail.next = head;
+            return;
+        }
+
+        do{
+            Node n = node.next;
+            if(n.val == val){
+                node.next = n.next;
+                break;
+            }
+            node = node.next;
+        }while(node!=head);
+
+    }
+
+//-----------------------------------------------------
+   private class Node{
+        int val;
+        Node next;
+
+    public Node(int val){
+        this.val = val;
+    }   
+    }
+//--------------------------------------------------
+    public static void main(String[] args){
+        CircularLL list = new CircularLL();
+        list.insert(3);
+        list.insert(30);
+        list.insert(67);
+        list.insert(89);
+        list.insert(9);
+        list.delete(67);
+        list.display();
+    } 
+}
