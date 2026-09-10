3f52c0deb419dbe385db41fd222c077f16b35355
vani-1289 <vs8648711@gmail.com>
2026-09-11 00:05:39 +0530

    Time: 10 ms (26.58%) | Memory: 47.5 MB (7.03%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int ans=0;
+        for(int i = 0;i<nums.length-1;i+=2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+
+          
+
+
+
+    }
+}
