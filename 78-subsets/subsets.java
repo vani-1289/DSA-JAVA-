d25b6654a246d621454034000346c090ebf01f0d
vani-1289 <vs8648711@gmail.com>
2026-09-15 22:06:30 +0530

    Time: 1 ms (87.24%) | Memory: 44.5 MB (6.96%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+
+        for(int num:nums){
+            int n = outer.size();
+            for(int i = 0;i<n;i++){
+                List<Integer> inner =new ArrayList<>(outer.get(i));
+                inner.add(num);
+                outer.add(inner);
+            }
+        }
+        return outer;
+        
+    }
+}
